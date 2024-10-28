a4ef891ae8a84c5805e5976c334b1e5992e53219
mdsajidul <ai.mdsajidulbd@gmail.com>
2024-10-28 18:36:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-document.getElementById('downloadBtn').addEventListener('click', function () {
-    const frontCard = document.getElementById('businessCardFront');
-    const backCard = document.getElementById('businessCardBack');
-    
-    html2canvas(frontCard, { scale: 2 }).then(function (canvas) { // scale: 2 to ensure high-quality PNG
-        canvas.toBlob(function (blob) {
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'business_card_front.png';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }, 'image/png'); // specify PNG format
-    });
-
-    html2canvas(backCard, { scale: 2 }).then(function (canvas) {
-        canvas.toBlob(function (blob) {
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'business_card_back.png';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }, 'image/png');
-    });
-});
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
