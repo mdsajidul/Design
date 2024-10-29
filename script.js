3d07e7a6a105c4d678dc9a380e1bd8b7125281d7
mdsajidul <ai.mdsajidulbd@gmail.com>
2024-10-29 08:26:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function searchGig() {
-    const searchQuery = document.getElementById('search').value;
-    if (searchQuery) {
-        alert(`You searched for "${searchQuery}".`);
-    } else {
-        alert('Please enter something to search.');
-    }
-}
 
-function viewService(service) {
-    alert(`You want to view details for the "${service}" service.`);
-}
